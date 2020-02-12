86149e9b9941cc1fc3bc7aaf7cfc46a2d190e63a
julikbob <juliabobyliova256@gmail.com>
2020-02-12 22:40:33 +0300

    Add cards on initialize

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-debugger;
+var global = {};
+global.templates = document.querySelector(".templates");
+global.cardsContainer = document.querySelector(".cards-container"); 
+global.hNumber = 7;
+global.vNumber = 8;
+
+function initCard(h, v, cardTemplate) {
+    global.cardsContainer
+        .appendChild(cardTemplate.cloneNode(true));
+};
+
+function initCards() {
+    var cardTemplate = global.templates
+        .querySelector(".card")
+        .cloneNode(true);
+    for(var i = 0; i < global.hNumber; ++i) {
+        for(var j = 0; j < global.vNumber; ++j) {
+            initCard(i, j, cardTemplate); 
+        }    
+    }
+};
+initCards();
